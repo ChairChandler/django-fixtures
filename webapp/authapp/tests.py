e314e8827cb912ae81f3e3ed603eda84e997c058
alewandowski <adam_lewandowski_1998@outlook.com>
2024-08-07 21:00:58 +0200

    created tests skeletons

--- webapp/authapp/tests.py
+++ webapp/authapp/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TelephoneModelTest(TestCase):
+    def test_auto_generated_ids(self):
+        pass
+
+    def test_phone_prefix_in_range(self):
+        pass
+
+    def test_phone_number(self):
+        pass
+
+    def test_prefix_with_phone_number_are_unique(self):
+        pass
+
+
+class AppUserModelTest(TestCase):
+    def test_email_is_username(self):
+        pass
+
+    def test_telephone_is_required(self):
+        pass
+
+    def test_email_is_unique(self):
+        pass
+
+    def test_email_format(self):
+        # username part: 64 chars
+        # @ part: 1 char
+        # domainame part: 255 chars
+        pass
+
+    def test_telephone_cannot_be_deleted(self):
+        # cannot be deleted
+        pass
+
+    def test_random_password_generation_with_email_sent(self):
+        pass
