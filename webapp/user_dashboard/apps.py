4522a0e0f180def1a2fc06afee9bc6bb4a6689c7
alewandowski <adam_lewandowski_1998@outlook.com>
2024-08-27 15:17:25 +0200

    moved reservation from signals to views

--- webapp/user_dashboard/apps.py
+++ webapp/user_dashboard/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class UserDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_dashboard'
-
-    def ready(self) -> None:
-        import user_dashboard.signals
--- webapp/user_dashboard/views.py
+++ webapp/user_dashboard/views.py
@@ -1,1 +1,40 @@
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpRequest
+from django.shortcuts import redirect, render
+import django.contrib.messages as messages
+from webapp.accounts.models import User
+from .models import ReservationUnit
+
 # Create your views here.
+
+
+@login_required
+def redirect_to_current_calendar(request: HttpRequest):
+    return redirect('')
+
+
+@login_required
+def show_calendar(request: HttpRequest, calendar_id: str):
+    return render(request, 'user_dashboard/calendar.html', {})
+
+
+@login_required
+@require_POST
+def check_reservation_participant(request: HttpRequest, reservation_id: str):
+    reservation = ReservationUnit.objects.get(pk=reservation_id)
+    if reservation.participant is not None:
+        messages.error(
+            request,
+            'Cannot change participant if another already set.'
+        )
+    else:
+        try:
+            user: User = request.user  # type: ignore
+            reservation.participant = user
+            reservation.save()
+            messages.success(request, 'Successfully joined')
+        except Exception:
+            messages.error(request, 'Cannot change participant')
+
+    return redirect(request.GET.get('next'))
