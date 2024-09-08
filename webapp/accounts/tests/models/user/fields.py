f5d8c4cf069e6c5bb5ffb9134af43767cff0dfaa
alewandowski <adam_lewandowski_1998@outlook.com>
2024-09-08 18:11:15 +0200

    finished fields tests

--- webapp/accounts/tests/models/user/fields.py
+++ webapp/accounts/tests/models/user/fields.py
@@ -19,22 +19,36 @@ class UuidFieldTest(ModelFieldTest, SimpleTestCase):
 
 
 class EmailFieldTest(ModelFieldTest, SimpleTestCase):
+    '''
+    - [1] email must be a username, being an user ID
+    - [2] email must be used once
+    - [3] email must pass email validator and the length must be less than 64 chars
+    '''
     model = User
     field_name = 'email'
+    # [3]
     field_type = models.EmailField
 
     verbose_name = 'email address'
+    #  [2]
     unique = True
+    # [3]
     max_length = 64
 
 
 class TelephonePrefixFieldTest(ModelFieldTest, SimpleTestCase):
+    '''
+    [1] Check if telephone prefix is in international range between 1 and 999.
+    It shoud fail if prefix is out of range.
+    '''
+
     model = User
     field_name = 'telephone_prefix'
     field_type = models.IntegerField
 
     verbose_name = 'telephone number prefix'
     default = 48
+    # [1]
     validators = [
         validators.MinValueValidator(1, "Minimum value must be 1"),
         validators.MaxValueValidator(999, "Maximum value must be 999")
@@ -43,14 +57,20 @@ class TelephonePrefixFieldTest(ModelFieldTest, SimpleTestCase):
 
 
 class TelephoneNumberFieldTest(ModelFieldTest, SimpleTestCase):
+    '''
+    [1] Telephone number is a 9-length texts of digits.
+    It should fail if length is different.
+    '''
     model = User
     field_name = 'telephone_number'
     field_type = models.CharField
 
     verbose_name = 'telephone number'
+    # [1]
     max_length = 9
     null = True
     blank = True
+    # [1]
     validators = [
         validators.MinLengthValidator(9, "Invalid telephone value")
     ]
@@ -116,8 +136,12 @@ class ObjectsFieldTest(ClassFieldTest, SimpleTestCase):
 # #
 
 class ConstraintsFieldTest(MetaFieldTest, SimpleTestCase):
+    '''
+    [1] Telephone number combined with prefix must be unique.
+    '''
     klass = User
     field_name = 'constraints'
+    # [1]
     value = [
         models.UniqueConstraint(
             fields=['telephone_prefix', 'telephone_number'],
